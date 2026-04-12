9c36af775717c8c572824b22cb877c8154c7edbf
VarCode <varadevworks@gmail.com>
2026-04-11 21:12:24 -0400

    Implement detab functionality to replace spaces with tabs based on tab width

--- exercises/chapter-01/ex-21.c
+++ exercises/chapter-01/ex-21.c
@@ -1,1 +1,75 @@
 #include <stdio.h>
+
+void detab(int tab_width);
+void print_blanks(int space_count);
+int calculate_next_tab_stop(int column, int tab_width);
+
+int main(void)
+{
+    int tab_width = 8;
+    detab(tab_width);
+}
+
+void detab(int tab_width)
+{
+    int ch;
+    int column = 0;
+    int next_tab_stop = 0;
+    int spaces_till_tab_stop = 0;
+    int pending_spaces = 0;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch == ' ')
+        {
+            next_tab_stop = calculate_next_tab_stop(column, tab_width);
+            spaces_till_tab_stop = next_tab_stop - column;
+            ++pending_spaces;
+            if (spaces_till_tab_stop == 1)
+            {
+                putchar('\t');
+                pending_spaces = 0; // Reset pending spaces after printing a tab
+            }
+            ++column;
+            continue;
+        }
+        // Flush pending spaces before printing the character
+        if (pending_spaces != 0)
+        {
+            print_blanks(pending_spaces);
+            pending_spaces = 0;
+        }
+        // Character: Tab : Set column to the next tab stop
+        if (ch == '\t')
+        {
+            column = calculate_next_tab_stop(column, tab_width);
+            pending_spaces = 0;
+        }
+        else
+            ++column;
+        // Character: Newline : Reset column and pending spaces
+        if (ch == '\n')
+        {
+            column = 0;
+            pending_spaces = 0;
+        }
+        putchar(ch);
+    }
+
+    if (pending_spaces != 0)
+        print_blanks(pending_spaces);
+}
+
+void print_blanks(int space_count)
+{
+    while (space_count > 0)
+    {
+        putchar(' ');
+        --space_count;
+    }
+}
+
+int calculate_next_tab_stop(int column, int tab_width)
+{
+    return ((column / tab_width) + 1) * tab_width;
+}
