8fe1ded254897f3bcdca060317fef78e8616a9de
VarCode <varadevworks@gmail.com>
2026-04-25 16:38:39 -0400

    Added: Exercise 02-01: Print Int, Float limits

--- exercises/chapter-02/ex-02-01.c
+++ exercises/chapter-02/ex-02-01.c
@@ -2,15 +2,71 @@
 #include <float.h>
 #include <stdio.h>
 
+signed long compute_signed_long_min(int num_bits);
+signed long compute_signed_long_max(int num_bits);
+unsigned long compute_unsigned_long_max(int num_bits);
+
 int main(void)
 {
-    printf("signed char : %d - %d\n", CHAR_MIN, CHAR_MAX);
-    printf("unsigned char : %d - %d\n", 0, UCHAR_MAX);
-    printf("signed short : %d - %d\n", SHRT_MIN, SHRT_MAX);
-    printf("unsigned short : %d - %d\n", 0, USHRT_MAX);
-    printf("signed int : %d - %d\n", INT_MIN, INT_MAX);
-    printf("unsigned int : %d - %d\n", 0, UINT_MAX);
-    printf("signed long : %ld - %ld", LONG_MIN, LONG_MAX);
-    printf("unsigned long : %ld - %ld", 0, ULONG_MAX);
+
+    // Int types limits
+    printf("signed char range (from standard header)\t: %d to %d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("signed char range (by direct computation)\t: %ld to %ld\n", compute_signed_long_min(sizeof(char) * CHAR_BIT), compute_signed_long_max(sizeof(char) * CHAR_BIT));
+
+    printf("unsigned char range (from standard header)\t: %d to %u\n", 0, UCHAR_MAX);
+    printf("unsigned char range (by direct computation)\t: %d to %lu\n", 0, compute_unsigned_long_max(sizeof(char) * CHAR_BIT));
+
+    printf("signed short range (from standard header)\t: %d to %d\n", SHRT_MIN, SHRT_MAX);
+    printf("signed short range (by direct computation)\t: %ld to %ld\n", compute_signed_long_min(sizeof(short) * CHAR_BIT), compute_signed_long_max(sizeof(short) * CHAR_BIT));
+
+    printf("unsigned short range (from standard header)\t: %d to %u\n", 0, USHRT_MAX);
+    printf("unsigned short range (by direct computation)\t: %d to %lu\n", 0, compute_unsigned_long_max(sizeof(short) * CHAR_BIT));
+
+    printf("signed int range (from standard header) \t: %d to %d\n", INT_MIN, INT_MAX);
+    printf("signed int range (by direct computation)\t: %ld to %ld\n", compute_signed_long_min(sizeof(int) * CHAR_BIT), compute_signed_long_max(sizeof(int) * CHAR_BIT));
+
+    printf("unsigned int range (from standard header)\t: %d to %u\n", 0, UINT_MAX);
+    printf("unsigned int range (by direct computation)\t: %d to %lu\n", 0, compute_unsigned_long_max(sizeof(int) * CHAR_BIT));
+
+    printf("signed long range (from standard header)\t: %ld to %ld\n", LONG_MIN, LONG_MAX);
+    printf("signed long range (by direct computation)\t: %ld to %ld\n", compute_signed_long_min(sizeof(long) * CHAR_BIT), compute_signed_long_max(sizeof(long) * CHAR_BIT));
+
+    printf("unsigned long range (from standard header)\t: %d to %lu\n", 0, ULONG_MAX);
+    printf("unsigned long range (by direct computation)\t: %d to %lu\n", 0, compute_unsigned_long_max(sizeof(long) * CHAR_BIT));
+
+    // floating point types limits
+    printf("float\t: %e to %e\n", FLT_MIN, FLT_MAX);
+    printf("double\t: %e to %e\n", DBL_MIN, DBL_MAX);
+
     return 0;
+}
+
+signed long compute_signed_long_min(int num_bits)
+{
+    signed long value = -1;
+
+    for (int i = 1; i < num_bits; i++)
+        value = 2 * value;
+
+    return value;
+}
+
+signed long compute_signed_long_max(int num_bits)
+{
+    unsigned long value = 1;
+
+    for (int i = 1; i < num_bits; i++)
+        value = 2 * value;
+
+    return --value;
+}
+
+unsigned long compute_unsigned_long_max(int num_bits)
+{
+    unsigned long value = 1;
+
+    for (int i = 1; i <= num_bits; i++)
+        value = 2 * value;
+
+    return --value;
 }
