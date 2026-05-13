9dc9a648f5db27b2dd1e39beac55b478f50fbb5b
VarCode <varadevworks@gmail.com>
2026-05-13 13:32:48 -0400

    Exercise: 4-3: Added -ve numbers and % remainder handling

---
+++ exercises/chapter-04/ex-04-03.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char s[]);
+void push(double f);
+double pop(void);
+
+int main(void)
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF)
+    {
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '%':
+            op2 = pop();
+            if (op2 != 0.0)
+                push((int)pop() % (int)op2);
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+
+    return 0;
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can`t push %g\n", f);
+}
+
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+
+    i = 0;
+
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c; /* not a number */
+
+    // handle negative numbers
+    if (c == '-')
+    {
+        s[++i] = c = getch();
+        if (!isdigit(c) && c != '.')
+        {
+            ungetch(c);
+            return s[i - 1];
+        }
+    }
+
+    if (isdigit(c)) /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+
+    if (c == '.') /* collect fractional part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0;      /* next free position in buffer */
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
