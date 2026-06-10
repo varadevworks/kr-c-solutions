36d1e07f680d88695b7d2eeb47d6e577f88e9fa1
Vara Tumpala <varadevworks@gmail.com>
2026-06-09 21:15:00 -0400

    Added: Exercise 05-05: strncpy function implementation. pending strncat, strncmp implementation

---
+++ exercises/chapter-05/ex-05-05.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+char *strncpy(char *s, const char *t, size_t n);
+char *strncat(char *s, const int *t, size_t n);
+
+int main(void)
+{
+    char t[] = "Hello";
+    char s[3];
+    size_t n = sizeof(s) - 1;
+
+    strncpy(s, t, n);
+    s[n] = '\0';
+
+    printf("output: %s\n", s);
+
+    return 0;
+}
+
+char *strncpy(char *s, const char *t, size_t n)
+{
+    char *o = s;
+
+    while (n > 0)
+    {
+        if (*s++ = *t++)
+            n--;
+        else /* t is shorter than n */
+        {
+            n--;
+            break;
+        }
+    }
+
+    /* if t is shorter than n then pad with null characters */
+    while (n > 0)
+    {
+        *s++ = '\0';
+        n--;
+    }
+
+    return o;
+}
+
+char *strncat(char *s, const int *t, size_t n){
+
+}
