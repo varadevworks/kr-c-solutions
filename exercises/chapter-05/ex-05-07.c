3fdbeee63b22664909ffc4980961a32bc007f0e2
VarCode <varadevworks@gmail.com>
2026-06-17 21:31:22 -0400

    updated: Exercise 05-07: Final version of readlines implementation

--- exercises/chapter-05/ex-05-07.c
+++ exercises/chapter-05/ex-05-07.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+
 #define MAXLINES 1000
 #define MAXLINELEN 500
 
-/* readlines: read input lines */
-int readlines(char *lineptr[], int maxlines);
-/* get_line: read input line*/
+int readlines(char lines[], int maxlines, size_t total_storage);
 int get_line(char *s, int lim);
 
 int main(void)
 {
     int nlines;
-    char *lineptr[MAXLINES];
-    char line[MAXLINELEN];
+    size_t total_storage = MAXLINES * MAXLINELEN;
+    char lines[total_storage];
 
-    // nlines = readlines(lineptr, MAXLINES);
-    // printf("no of input lines read: %d", nlines);
-    nlines = get_line(line, MAXLINELEN);
-    printf("line: length %d , data %s", nlines, line);
+    nlines = readlines(lines, MAXLINES, total_storage);
+    printf("no of input lines read: %d", nlines);
 
     return 0;
+}
+
+/* readlines: read input lines */
+int readlines(char lines[], int maxlines, size_t total_storage)
+{
+    int len, nlines;
+    char line[MAXLINELEN];
+    char *lineptr = lines;
+
+    nlines = 0;
+
+    while ((len = get_line(line, MAXLINELEN)) > 0)
+    {
+        if (nlines >= maxlines || (int)total_storage < ((int)(lineptr - lines) + len))
+            return -1;
+        else
+        {
+            if (line[len - 1] == '\n')
+                line[len - 1] = '\0';
+            strcpy(lineptr, line);
+            lineptr += len;
+            nlines++;
+        }
+    }
+    return nlines;
 }
 
 int get_line(char *s, int lim)
